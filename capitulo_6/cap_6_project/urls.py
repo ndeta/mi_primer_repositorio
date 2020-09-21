28cf1b2b806d4d8f50fa10780bde63e9d2e24cea
ndeta <ndeyambayata@gmail.com>
2020-09-21 16:10:43 +0200

    formularios_1

--- capitulo_6/cap_6_project/urls.py
+++ capitulo_6/cap_6_project/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.contrib import admin
+from django.urls import path
+from listas import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('formularios/',views.formularios, name='formularios'),
 ]
