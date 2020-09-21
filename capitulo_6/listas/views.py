398b755032cfd62d818d35c02d7fd6cafbdcb00e
ndeta <ndeyambayata@gmail.com>
2020-09-21 20:55:22 +0200

    formulario_·

--- capitulo_6/listas/views.py
+++ capitulo_6/listas/views.py
@@ -5,12 +5,14 @@ import psycopg2
 def formularios(request):
     return render(request, 'formularios.html')
 def anadir(request):
-    prioridad = request.POST["name_prioridad"]
-    titulo = request.POST["nombre_titulo"]
-    nota = request.POST["name_nota"]
 
+    conn = psycopg2.connect(dbname="capitulo_6_db",
+                            user="capitulo_6_user",
+                            password="mango")
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO nota VALUES ('total','coregir_examenes', \
+                       'examenes_uf3_lunes');")
+    cursor.execute("select * from nota")
+    return HttpResponse(cursor.fetchall())
 
-    return(HttpResponse(f'Insertado <br> '
-                        f'prioridad: {prioridad }<br>'
-                        f'titulo: {titulo}<br>'
-                        f'nota: {nota}<br>'))
+    return HttpResponse('registros insertados')
