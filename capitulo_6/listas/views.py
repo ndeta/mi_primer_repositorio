22bd1295e53da4e839b09dfcbeed95e4a28231a8
ndeta <ndeyambayata@gmail.com>
2020-09-22 17:21:48 +0200

    formularios_4

--- capitulo_6/listas/views.py
+++ capitulo_6/listas/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render,HttpResponse
+from django.shortcuts import redirect
 import psycopg2
 
 # Create your views here.
 def formularios(request):
     return render(request, 'formularios.html')
+
+
 def anadir(request):
 
     conn = psycopg2.connect(dbname="capitulo_6_db",
@@ -13,6 +16,8 @@ def anadir(request):
     cursor.execute("INSERT INTO nota VALUES ('total','coregir_examenes', \
                        'examenes_uf3_lunes');")
     cursor.execute("select * from nota")
-    return HttpResponse(cursor.fetchall())
+    conn.commit()
+    cursor.close()
+    conn.close()
 
-    return HttpResponse('registros insertados')
+    return redirect('formularios')
