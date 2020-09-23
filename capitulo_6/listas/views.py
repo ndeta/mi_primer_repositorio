71cdcd524c3793a91a2911d4c81467ab582e9d88
ndeta <ndeyambayata@gmail.com>
2020-09-23 16:09:31 +0200

    nota_fin

--- capitulo_6/listas/views.py
+++ capitulo_6/listas/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,HttpResponse
 from django.shortcuts import redirect
 import psycopg2
+import psycopg2.extras
 
 # Create your views here.
 def formularios(request):
@@ -9,9 +10,10 @@ def formularios(request):
                             user="capitulo_6_user",
                             password="mango")
     cursor = conn.cursor()
+    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
     cursor.execute("SELECT * FROM Nota;")
-    result = cursor.fetchone()
+    result = cursor.fetchall()
     cursor.close()
     conn.close()
     params = {'notas': result}
@@ -23,6 +25,8 @@ def anadir(request):
     conn = psycopg2.connect(dbname="capitulo_6_db",
                             user="capitulo_6_user",
                             password="mango")
+
+
     cursor = conn.cursor()
     prioridad = request.POST["name_prioridad"]
     titulo= request.POST["nombre_titulo"]
